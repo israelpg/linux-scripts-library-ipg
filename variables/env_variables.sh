0571938f39d1251f14afd07f1b98cb6c3f102a38
root <root@02DI20161235444.net1.cec.eu.int>
2018-07-23 13:30:05 +0200

    update from mloz on 23/07/18

--- variables/env_variables.sh
+++ variables/env_variables.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
+## user env variable, within ~
+.bashrc --> effective when a new local terminal is opened just for ~ user who has this setup
+.bash_profile --> applicable for remote login
+.bash_login
+.profile
+
+# system wide env variables
+/etc/bash.bashrc --> for all users but only local sessions
+/etc/environment --> for every user, local and remote sessions (e.g.: defining proxy var settings)
+/etc/profile --> better use profile.d
+/etc/profile.d --> this is related with applications/scripts execution, like for instance: vim.sh
+
+# setting vars in a shell:
+set varname
+# or:
+export varname
+# to take effect/refresh:
+source .bashrc # or the applicable var name
+# unset a var:
+unset varname
+
+# in a terminal you can also temp unset env vars:
+env -i bash
+# to restore env vars in this session again:
+exit
+
 # if you have an application installed in /opt (like maven), you must export some values to env variables, concerning path for instance
 
 # check: mavenenv.sh
