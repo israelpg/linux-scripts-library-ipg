594f2b2e03f8ea6f90c4bc5b36e399604067d1e4
israelpg <israelpg@gmail.com>
2019-10-29 21:59:59 +0100

    adding more python

---
+++ python_practices/check_status2.py
@@ -0,0 +1,5 @@
+var1 = eval(input('Enter a number: '))
+
+status = True if var1 > 5 else False
+
+print('Well done, approved') if status == True else print('Butthead')
---
+++ python_practices/condition_within_print3.py
@@ -0,0 +1,3 @@
+score=eval(input('Enter score for this player:'))
+
+print('This player was ', 'awesome' if score > 20 else 'not a star today')
